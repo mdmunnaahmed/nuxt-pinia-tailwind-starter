7d5d38473b4e6907df6c6e3fb70d4f05ebad00cd
mdmunnaahmned <msdmunna77@gmail.com>
2023-12-25 12:04:24 +0600

    left for project

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="container mx-auto mt-10 mb-10">
     <h2 class="hidden md:block">Home Pgae</h2>
+    {{ myorder }}
+    <button class="bg-red-500" @click="fetchOrders">Insert</button>
   </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+const supabase = useSupabaseClient();
+const myorder = ref(null);
+const fetchOrders = async () => {
+  try {
+    const { data: order, error } = await supabase.from("users").insert({
+      name: "manna",
+    });
+    if (error) {
+      console.log(error.code);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 </script>
