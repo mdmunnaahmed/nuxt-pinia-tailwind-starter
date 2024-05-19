1318f17ee2f6f8817a679b3f403cd3ab72cef9f6
mdmunnaahmed <msdmunna77@gmail.com>
2024-05-19 18:35:46 +0600

    started on register --back

--- app.vue
+++ app.vue
@@ -7,7 +7,7 @@
 </template>
 
 
-<script setup lang="ts">
+<script setup>
 useHead({
   link: [
     {
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,42 @@
+import { defineNuxtConfig } from "nuxt/config";
+
 export default defineNuxtConfig({
-  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss", '@nuxtjs/supabase','shadcn-nuxt'],
-  pinia: { autoImports: ["defineStore", ["defineStore", "definePiniaStore"]] },
-  // css: ["@/assets/main.scss"],
-  css: [
-    { src: "~/assets/_mixins.scss", lang: "scss" },
-    { src: "~/assets/_extend.scss", lang: "scss" },
-    { src: "~/assets/main.scss", lang: "scss" },
-  ],
-  shadcn: {
-    /**
-     * Prefix for all the imported component
-     */
-    prefix: '',
-    /**
-     * Directory that the component lives in.
-     * @default "./components/ui"
-     */
-    componentDir: './components/ui'
+  app: {
+    head: {
+      link: [
+        { rel: "stylesheet", href: "/css/vendor/bootstrap.min.css" },
+        { rel: "stylesheet", href: "/css/vendor/font-awesome.css" },
+        { rel: "stylesheet", href: "/css/vendor/slick.css" },
+        { rel: "stylesheet", href: "/css/vendor/slick-theme.css" },
+        { rel: "stylesheet", href: "/css/vendor/sal.css" },
+        { rel: "stylesheet", href: "/css/vendor/magnific-popup.css" },
+        { rel: "stylesheet", href: "/css/vendor/green-audio-player.min.css" },
+        { rel: "stylesheet", href: "/css/vendor/odometer-theme-default.css" },
+        { rel: "stylesheet", href: "/css/app.css" },
+      ],
+
+      
+      script: [
+        { src: "/js/vendor/jquery-3.6.0.min.js", body: true, defer: true },
+        { src: "/js/vendor/bootstrap.min.js", body: true, defer: true },
+        { src: "/js/vendor/isotope.pkgd.min.js", body: true, defer: true },
+        { src: "/js/vendor/imagesloaded.pkgd.min.js", body: true, defer: true },
+        { src: "/js/vendor/odometer.min.js", body: true, defer: true },
+        { src: "/js/vendor/jquery-appear.js", body: true, defer: true },
+        { src: "/js/vendor/slick.min.js", body: true, defer: true },
+        { src: "/js/vendor/sal.js", body: true, defer: true },
+        { src: "/js/vendor/jquery.magnific-popup.min.js", body: true, defer: true },
+        { src: "/js/vendor/js.cookie.js", body: true, defer: true },
+        { src: "/js/vendor/jquery.style.switcher.js", body: true, defer: true },
+        { src: "/js/vendor/jquery.countdown.min.js", body: true, defer: true },
+        { src: "/js/vendor/tilt.js", body: true, defer: true },
+        { src: "/js/vendor/green-audio-player.min.js", body: true, defer: true },
+        { src: "/js/vendor/jquery.nav.js", body: true, defer: true },
+      ]
+    }
   },
+  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss", '@nuxtjs/supabase'],
+  pinia: { autoImports: ["defineStore", ["defineStore", "definePiniaStore"]] },
   supabase: {
     redirect: false
   }
